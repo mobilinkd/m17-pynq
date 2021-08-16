3e8fc8acc1625fe3c4c7ebb8fc8e33e031dbd189
Rob Riggs <rob@pangalactic.org>
2021-08-15 20:02:01 -0500

    Add more missing files.

---
+++ Vivado/acg/acg.gen/sources_1/bd/design_1/ip/design_1_selectio_wiz_1_0/design_1_selectio_wiz_1_0_stub.v
@@ -0,0 +1,24 @@
+// Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2021.1 (lin64) Build 3247384 Thu Jun 10 19:36:07 MDT 2021
+// Date        : Sun Aug 15 11:11:15 2021
+// Host        : emerald running 64-bit Fedora release 33 (Thirty Three)
+// Command     : write_verilog -force -mode synth_stub
+//               /home/rob/m17-pynq/Vivado/acg/acg.gen/sources_1/bd/design_1/ip/design_1_selectio_wiz_1_0/design_1_selectio_wiz_1_0_stub.v
+// Design      : design_1_selectio_wiz_1_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+module design_1_selectio_wiz_1_0(data_out_from_device, data_out_to_pins, 
+  clk_in, clk_div_in, io_reset)
+/* synthesis syn_black_box black_box_pad_pin="data_out_from_device[7:0],data_out_to_pins[0:0],clk_in,clk_div_in,io_reset" */;
+  input [7:0]data_out_from_device;
+  output [0:0]data_out_to_pins;
+  input clk_in;
+  input clk_div_in;
+  input io_reset;
+endmodule
